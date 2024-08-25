7aeadc9ca4bdd955e0762a1408f7b5e12d59eb04
Andre Justin <andrejustin77@gmail.com>
2024-08-25 20:23:13 +0700

    first commit

---
+++ src/components/Events/NewEvent.jsx
@@ -0,0 +1,52 @@
+import { Link, useNavigate } from "react-router-dom";
+
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { useMutation } from "@tanstack/react-query";
+import { createNewEvent } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+
+export default function NewEvent() {
+  const navigate = useNavigate();
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+  }); // sbnrnya pakai useQuery bisa untuk post data tp, mutation udah optimize, dan galgsg ke execute seperti useQuery saat pertama kali render
+
+  function handleSubmit(formData) {
+    mutate(
+      { event: formData },
+      {
+        onSuccess: () => {
+          navigate("../");
+        },
+      }
+    );
+  }
+
+  return (
+    <Modal onClose={() => navigate("../")}>
+      <EventForm onSubmit={handleSubmit}>
+        {isPending && "Submitting..."}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
+      </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="Failed to create event"
+          message={
+            error.info?.message ||
+            "Failed to crate event. Please check your inputs and try again later. "
+          }
+        />
+      )}
+    </Modal>
+  );
+}
---
+++ src/components/Events/NewEventsSection.jsx
@@ -0,0 +1,69 @@
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvents } from "../../util/http.js";
+
+export default function NewEventsSection() {
+  // data is the result or return from the queryFn
+  // isPending is a boolean that indicates if the query is still pending
+  // isError is a boolean that indicates if the query has failed, tp harus di throw dulu di queryFn
+  // error is the error object that was thrown in the queryFn
+  // jadi kalau misalnya berhasil masuk ke data, kalau gagal masuk ke error
+
+  // ADVANTAGES OF USING REACT QUERY
+  // 1. Automatic caching
+  // 2. Automatic refetching
+  // 3. Automatic stale state management
+
+  const {
+    data,
+    isPending: isLoading,
+    isError,
+    error,
+  } = useQuery({
+    // fetch events will be executed by useQuery
+    queryKey: ["events"], // unique key for the query, for caching
+    queryFn: fetchEvents, // function that return a promise
+    // react query by default pass data jadi search termnya kedeteksi sebuah object.
+    // staleTime: 0, // selalu request behind the scene untuk updated data
+    staleTime: 5000, // jadi saat pindah halaman, dan kembali lagi ke halaman ini, data akan di fetch ulang setelah 5 detik
+    // gcTime: 60000, // garbage collection time, data yg sudah tidak terpakai akan dihapus in certain time
+  });
+
+  let content;
+
+  if (isLoading) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred!"
+        message={error.info?.message || "Failed to fetch events"}
+      />
+    ); // ini property yg di set saat throw error di fetchEvents
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <section className="content-section" id="new-events-section">
+      <header>
+        <h2>Recently added events</h2>
+      </header>
+      {content}
+    </section>
+  );
+}
